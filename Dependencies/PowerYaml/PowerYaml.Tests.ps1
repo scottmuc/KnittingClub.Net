5c586038ca8f995e4e07e3845e10a7234e68ea33
Scott Muc <root@scottmuc.com>
2011-02-13 12:59:09 -0700

    readded PowerYaml

---
+++ Dependencies/PowerYaml/PowerYaml.Tests.ps1
@@ -0,0 +1,31 @@
+Import-Module .\PowerYaml.psm1
+. ..\Pester\Source\Pester.ps1
+
+Describe "PowerYaml API (Get-Yaml)" {
+
+    It "Obtains a HashTable given a yaml hash" {
+        $yaml = Get-Yaml -YamlString "key: value"
+        $yaml.GetType().Name.should.be("HashTable")
+    }
+
+    It "Obtains an Object[] given a yaml array" {
+        $yaml = Get-Yaml -YamlString "- test
+- test2"
+        $yaml.GetType().Name.should.be("Object[]")
+    }
+
+    It "Can parse a yaml file without specifying a ypath" {
+        $yaml_file = Resolve-Path Examples\sample.yml
+        $yaml = Get-Yaml -YamlFile $yaml_file
+        $yaml.keys.count.should.be(1)
+    }
+
+    It "Can parse a yaml file and navigate to a subsection" {
+        $yaml_file = Resolve-Path Examples\sample.yml
+        $yaml = Get-Yaml -YamlFile $yaml_file -ypath parent, child
+        $yaml.keys.count.should.be(3)  
+    }
+
+}
+
+Remove-Module PowerYaml
