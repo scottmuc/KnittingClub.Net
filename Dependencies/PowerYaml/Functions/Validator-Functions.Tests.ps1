f7bbf28926bd9066cc324c460ffc66dd61c29743
Scott Muc <root@scottmuc.com>
2011-02-13 14:13:14 -0700

    I give up on submodules

---
+++ Dependencies/PowerYaml/Functions/Validator-Functions.Tests.ps1
@@ -0,0 +1,31 @@
+$pwd = Split-Path -Parent $MyInvocation.MyCommand.Path
+. "$pwd\Validator-Functions.ps1"
+. "$pwd\..\Pester\Source\Pester.ps1"
+
+Describe "Detect-Tab" {
+
+    It "should return true if a TAB character is found in text" {
+        $result = Detect-Tab "   `t     "
+        $result.should.be($true)
+    }
+
+    It "should return false if no TAB character is found in text" {
+        $result = Detect-Tab "          "
+        $result.should.be($false)
+    }
+
+}
+
+Describe "Validate-File" {
+
+    It "should return false if a file does not exist" {
+        $result = Validate-File "some non existent file"
+        $result.should.be($false)
+    }
+
+    It "should return true for a file that does exist and does not contain a TAB character" {
+        $result = Validate-File (Resolve-Path Examples\sample.yml)
+        $result.should.be($true)
+    }
+
+}
---
+++ Dependencies/PowerYaml/PowerYaml.psm1
@@ -0,0 +1,46 @@
+. $PSScriptRoot\Functions\YamlDotNet-Integration.ps1
+. $PSScriptRoot\Functions\Validator-Functions.ps1
+    
+<# 
+ .Synopsis
+  Returns an object that can be dot navigated
+
+ .Parameter YamlFile
+  File reference to a yaml document
+
+ .Parameter YamlString
+  Yaml string to be converted
+
+ .Parameter ypath
+  A comma delimitted list of nodes to traverse
+#>
+function Get-Yaml([string] $YamlString = "", [string] $YamlFile = "", $ypath = "") 
+{
+    $yaml = $null
+
+    if ($YamlString -ne "")
+    {
+        # TODO add yaml string/document validation
+        $yaml = Get-YamlDocumentFromString $YamlString
+    } 
+    elseif ($YamlFile -ne "")
+    {
+        if ((Validate-File $YamlFile)) {
+            $yaml = Get-YamlDocument -file $YamlFile
+        }
+    }
+
+    $nodes = $yaml.RootNode
+    Foreach($p in $ypath)
+    {
+        $nodes.Children.Keys |
+            Where-Object { $_.Value -eq $p } | 
+            % { $nodes = $nodes.Children[$_] }
+    }
+
+    return Explode-Node $nodes
+}
+
+
+Load-YamlDotNetLibraries (Join-Path $PSScriptRoot -ChildPath "Libs")
+Export-ModuleMember -Function Get-Yaml 
